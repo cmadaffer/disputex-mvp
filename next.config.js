278312a44609fdf6098c1241511c4fd975f5b5be
cmadaffer <curtismadaffer@gmail.com>
2025-07-24 18:56:23 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
+  },
+};
+
+module.exports = nextConfig;
