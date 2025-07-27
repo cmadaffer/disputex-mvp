f1a9407313b34a40457f885ca539411459824e7b
cmadaffer <curtismadaffer@gmail.com>
2025-07-27 15:38:13 -0400

    Update signup.js

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,21 +1,20 @@
-// /pages/api/signup.js
-import supabase from '../../lib/supabaseClient';
+import { createClientSupabaseClient } from '../../lib/supabaseClient'
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
+    return res.status(405).end()
   }
 
-  const { email, password } = req.body;
+  const { email } = req.body
+  const supabase = createClientSupabaseClient()
 
-  const { data, error } = await supabase.auth.signUp({
-    email,
-    password,
-  });
+  const { data, error } = await supabase
+    .from('waitlist')
+    .insert([{ email }])
 
   if (error) {
-    return res.status(400).json({ error: error.message });
+    return res.status(500).json({ error: error.message })
   }
 
-  return res.status(200).json({ user: data.user });
+  res.status(200).json({ success: true })
 }
